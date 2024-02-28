afdd9dff5b9e138e5044f988d0c0217a8e7e0b41
kumaranMba <kumarandinesh0411@gmail.com>
2024-02-28 19:14:58 +0530

    stateful component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,45 @@
-import { Component } from "react";
+import React, { Component } from 'react';
 
-class App extends Component{
-  // class component lifecycle
+class App extends Component {
 
-  //It called after the component is render 
-  componentDidMount(){
-    console.log("App mounted");
+  constructor(props){
+    super(props);// to call the parent class constructor
+    this.state={
+      count:0
+    }
   }
 
-  // It will called after the  props or state changes
-  componentDidUpdate(){
-    console.log("App is rerender/update");
-  }
-  
-  componentWillUnmount(){
-    // it will  call when a component is removed from DOM
-    // it will called before the component is removed from the Dom
-    // it will be detach the events  and clean up the data
-    // kindly refer the sir git hub 
+  increment =()=>{
+    this.setState({
+      count:this.state.count + 1
+    })
   }
 
-  render(){
-    return(
+  decrement =()=>{
+    this.setState({
+      count:this.state.count-1
+    })
+  }
+  reset =()=>{
+    this.setState({
+      count:0
+    })
+  }
+
+  render() {
+    return (
       <div>
-        <p>Hello World!</p>
+        <p>count:{this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+        <button onClick={this.reset}>Reset</button>  
       </div>
     )
   }
 }
 
-export default App;
+export default App
+
+
 
 
