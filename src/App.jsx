f99ba36996b734352815bae8211fc9748b3d255a
kumaranMba <kumarandinesh0411@gmail.com>
2024-02-28 09:01:46 +0530

    stateless component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import React from 'react'; //not necessay , from react 17.
+
+// It is a functional component or stateless component
+// It is just a javascript function
+// It  needs to return JSX (Javascript XML)=> HTML and javascript embeded. 
 
 function App() {
   return (
