4faee7585cf660b7e0fe07bdb9e310101d8e9c79
kumaranMba <kumarandinesh0411@gmail.com>
2024-03-01 21:05:24 +0530

    Adding function using a function of useState rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class App extends Component {
+function App() {
+  // using useState and rendering the state condition.
+  const [count,setCount]= useState(0);
+ 
+  setTimeout(()=>{
+    setCount(count+1);
+  },1000);
 
-  constructor(props){
-    super(props);// to call the parent class constructor
-    this.state={
-      count:0
-    }
-  }
+  console.log("Render",count);
 
-  increment =()=>{
-    this.setState({
-      count:this.state.count + 1
-    })
-  }
-
-  decrement =()=>{
-    this.setState({
-      count:this.state.count-1
-    })
-  }
-  reset =()=>{
-    this.setState({
-      count:0
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <p>count:{this.state.count}</p>
-        <button onClick={this.increment}>Increment</button>
-        <button onClick={this.decrement}>Decrement</button>
-        <button onClick={this.reset}>Reset</button>  
-      </div>
-    )
-  }
+  return (
+    <div>App</div>
+  )
 }
 
-export default App
+export default App;
 
 
 
