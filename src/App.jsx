854e9c69cc853b94c381c5773057f0fbacfb2c0b
kumaranMba <kumarandinesh0411@gmail.com>
2024-03-02 17:28:35 +0530

    using UseEffect how does the dependency list will works

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
 import React, { useEffect, useState } from 'react';
 
 function App() {
- const [posts,setPosts] = useState(null);
- console.log(posts);
 
-  const fetchData = async () =>{
-   let response = await fetch(`https://jsonplaceholder.typicode.com/posts`)
-    let data = await response.json();
-    setPosts(data);
-   }
-  
+  const[count,setCount] = useState(0);
+
   useEffect(()=>{
-     fetchData();
-  },[])
+    document.title =`count:${count}`
+  },[count])
 
+  const Increase = () =>{
+    setCount(count+1);
+  }
+
+  
   return (
     <div>
-    <h1>API Notes</h1>
-    <ul>
-      {
-        posts? (posts.map((post)=>(
-          <li key ={post.id}><b>Title:</b>{post.title}<br/><b>Body:</b>{post.body}</li>
-
-         
-          ))):(<p>fetching data...</p>)
-      }
-    </ul>
+    <p>Count:{count}</p>
+      <button onClick={Increase}>IncreaseCount</button>
     </div>
   )
 }
 
 export default App;
-
 
