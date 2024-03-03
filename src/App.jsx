b82da2670e6265244bae1ddb9176fd0c1edd6001
kumaranMba <kumarandinesh0411@gmail.com>
2024-03-03 20:17:45 +0530

    Passing data from the child to parent component

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function GrandChild({data}){
-  console.log(`From Grand Child:${data}`);
+function ChildComponent({receivingData}){
+
+  let data ="Hi,This is from Child Component"
+
+ const buttonClick = () =>{
+  receivingData(data);
+ }
+
   return(
     <div>
-        <h5>Grand Child</h5>
-        
-    </div>
-  )
-}
-
-function ChildComponent({data}){
-  return(
-    <div>
-      <h5>Child Component</h5>
-      <GrandChild data ={data}/>
+    <button onClick={buttonClick}>pressPropsToParent</button>
     </div>
   )
 }
 
 function App() {
-  const parentComponent = `Hello from parent`;
+
+  const [dataFromChild,setDataFromChild] = useState('');
+
+    const ReceivingFromChild = (data) =>{
+      setDataFromChild(data);
+    }
+
   return (
     <div>
-      <h5>Parent  Component</h5>
-      <ChildComponent data ={parentComponent}/>
+      <h1>Hello World!</h1>
+      <p>This is a parent Component</p>
+      <p>`Receiving from the Child:{dataFromChild}`</p>
+      <ChildComponent receivingData ={ReceivingFromChild}/>
     </div>
   )
 }
 
 export default App;
-
