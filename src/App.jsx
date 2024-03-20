0620546270a58f1f83807124a88dd0b32704fe34
kumaranMba <kumarandinesh0411@gmail.com>
2024-03-20 11:34:33 +0530

    useRef hook is used for reference to the element

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useRef } from 'react';
+
+// useRef
+// Its a react's build in feature.
+// To create and manage reference of DOM elements.
+
+
 
 function App() {
+  const inputRef = useRef(null); // creating a reference object.
+
+  const handleinput =() =>{
+    // console.log(inputRef.current.value);
+    // inputRef.current.value ="dinesh";
+    // inputRef.current.focus();
+  }
+
+  // Another  way to use the function call at the time of loading
+  useEffect(()=>{
+    handleinput();
+  },[]);
+
   return (
     <div>
-          App
+     <input type="text" placeholder='type a message' ref={inputRef} />
+     <button>Focus input</button>
+
+
     </div>
   )
 }
