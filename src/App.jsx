b0ebab29e80a40195fa4a095ff4dd731a8135549
kumaranMba <kumarandinesh0411@gmail.com>
2024-03-21 09:38:09 +0530

    Useing useState to understand useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,38 @@
 import React from 'react';
-import { useRef } from 'react';
+import { useState } from 'react';
 
 function App() {
 
-  const rows = useRef();
+  const[count,setCount] = useState(0);
+  const[clickHistory,setClickHistory] = useState([]);
 
- const changeBackground =()=>{
-     const rowsRef = rows.current.querySelectorAll(" tbody tr");
-     rowsRef.forEach((row,index)=>{
-      console.log(index);
-        if(index % 2 === 0){
-          row.style.backgroundColor="green";
-        }
-     })
- }
+  const Increment =() =>{
+    setCount(count+1);
+    setClickHistory(clickHistory.concat('Incr'));
+  }
+  
+  const Decrement =() =>{
+    if(count > 0){
+      setCount(count-1);
+      setClickHistory(clickHistory.concat('Decr'));
+    }
+  }
+
+  const Reset = ()=>{
+    setCount(0);
+    setClickHistory(clickHistory.concat('Reset'));
+  }
 
   return (
     <div>
-          <table ref={rows}>
-              <thead>
-                 <tr>
-                  <th>SINO</th>
-                  <th>Name</th>
-                  <th>Age</th>
-                 </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>Dinesh</td>
-                  <td>25</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Kumaran</td>
-                  <td>30</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Kumaran</td>
-                  <td>30</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Kumaran</td>
-                  <td>30</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Kumaran</td>
-                  <td>30</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Kumaran</td>
-                  <td>30</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Kumaran</td>
-                  <td>30</td>
-                </tr>
-              </tbody>
-          </table>
+        <p>Count:{count}</p>
+        <button onClick={Increment}>Increment</button>
+        <button onClick={Decrement}>Decrement</button>
+        <button onClick={Reset}>Reset</button>
 
-          <button onClick={changeBackground}>change tr background</button>
+        <h5>Track user Interaction history</h5>
+        <p>{clickHistory.join(' ')}</p>
+
     </div>
   )
 }
