083fc90e680c58dc6b1beae952f55715abf533af
kumaranMba <kumarandinesh0411@gmail.com>
2024-03-22 20:15:44 +0530

    tic tac toe game

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,84 @@
-import React from 'react';
-import { useReducer } from 'react';
- 
-// create the initial state
+import React, { useState } from 'react';
+import "./Styles/App.css";
 
-const initialState = {
-  value:" ",
+function Button ({value,onClick}) {
+  return(<button className='square' onClick={onClick}>{value}</button>);
 }
-
-//define reducer
-
-const inputChange = (state,action)=>{
-  switch(action.type){
-      case"change":
-        return{value:action.payload};
-        
-      default:
-        return state;
-  }
-}
-
-
 
 function App() {
+  const[square,setSquare] = useState(Array(9).fill(null));
+  const[XIsNext,setXIsNext] = useState(true);
 
-  const [state,discharge] = useReducer(inputChange,initialState);
+  const handleClick = (i) => {
+    console.log(i,"clicked");
+
+    
+    const nextSquare = square.slice();
+    if(square[i] || checkWinner(square) ) {
+      return;
+    }
+    
+    if(XIsNext){
+      nextSquare[i] = "X";
+    }else{
+      nextSquare[i] = "O";
+    }
+
+    setSquare(nextSquare);
+    setXIsNext(!XIsNext);
+    
+    console.log(square);
+    
+  }
+  const checkWinner = (square) =>{
+      const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+      ];
+
+      for(let i=0; i<lines.length; i++){
+          const [a, b, c] = lines[i];
+        if(square[a] && square[a] == square[b] && square[a] == square[c]){
+          return square[a];
+        }
+      }
+      
+  }
+
+  let winner = checkWinner(square);
+  let status;
+  if(winner){
+    status = "winner" + winner;
+  }else{
+    status = "Next player:" +(XIsNext? "X":"O");
+  }
+
 
   return (
+        
     <div>
-         <input type='text' placeholder='type any text' onChange={(e)=>discharge({type:"change",payload:e.target.value})}/>
-         <p>value:{state.value}</p>
+        <p>{status}</p>
+        <div className='row'>
+        <Button value={square[0]} onClick ={()=>handleClick(0)}/>
+        <Button value={square[1]} onClick ={()=>handleClick(1)}/>
+        <Button value={square[2]} onClick ={()=>handleClick(2)}/>
+        </div>
+        <div className='row'>
+        <Button value={square[3]} onClick ={()=>handleClick(3)}/>
+        <Button value={square[4]} onClick ={()=>handleClick(4)}/>
+        <Button value={square[5]} onClick ={()=>handleClick(5)}/>
+        </div>
+        <div className='row'>
+        <Button value={square[6]} onClick ={()=>handleClick(6)}/>
+        <Button value={square[7]} onClick ={()=>handleClick(7)}/>
+        <Button value={square[8]} onClick ={()=>handleClick(8)}/>
+        </div>
     </div>
   )
 }
