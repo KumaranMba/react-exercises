4207c26d003dd803acf90733969359bf1d5e9abb
kumaranMba <kumarandinesh0411@gmail.com>
2024-03-26 20:32:03 +0530

    Adding a new note as input

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 function App(props) {
-  console.log(props);
+  // console.log(props);
   const[notes,setNotes] = useState(props.notes);
+  
+  // const {notes} = props;
+  const newNoteContentRef = useRef(null);
+  const[newNoteasinput,setnewNoteasInput] = useState(' ');
+  console.log(newNoteasinput);
 
   return (
     <div> 
@@ -12,6 +17,21 @@ function App(props) {
           return<li key= {note.id}>{note.content}</li>
 
           })}</ul>
+          <hr></hr>
+          <h1>Add a content</h1>
+          <form>
+            <label htmlFor="newNote">
+              Content: &nbsp;&nbsp;&nbsp;
+            </label>
+            <input id='newNote' 
+            type='text' 
+            placeholder='Enter a content' 
+            ref={newNoteContentRef}
+            value={newNoteasinput}
+            onChange={e=> setnewNoteasInput(e.target.value)}
+            />
+          </form>
+
     </div>
   )
 }
